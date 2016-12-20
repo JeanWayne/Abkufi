58ba4eb65c9a4e37a9dd4cdfc984f8a0698a6fa4
charbonn <jean.charbonnier@hs-hannover.de>
2016-12-20 11:38:54 +0100

    added improved change. Added Regex to remove math forumlas

--- Main.py
+++ Main.py
@@ -22,11 +22,12 @@ def hasNumbers(inputString):
 
 client = MongoClient('localhost', 27017)
 db = client['Papers']
-minlength = 0
-collection = db['hindawi_1480672900295']
-tonkenz = db['Tokenz_1480672900295_4']
+minlength = 1
+collection = db['hindawi_1481807503629']
+tonkenz = db['tokenz_hindawi_1481807503629']
 num_cores = multiprocessing.cpu_count()
 
+skip = 0
 
 #tonkenz.create_index([("Token", pymongo.ASCENDING)],unique=True)
 #cursor = collection.find({"Title":"Selected Papers from TimeNav 2007"})
@@ -49,7 +50,7 @@ num_cores = multiprocessing.cpu_count()
 ############################################
 year="2010"
 
-count = collection.find({"Year": ""+year+""}).count()
+count = collection.find({"year": ""+year+""}).count()
 print("size " + str(count))
 #cursor.add_option(no_cursor_timeout=True)
 #cursor.batch_size(3)
@@ -71,15 +72,15 @@ iucnn =codecs.open("isUpperCaseNoNumbers", "w", "utf-8")
 # ” this char can not be read
 punct = [line.rstrip('\n') for line in open('punctuation')]
 namendE = [line.rstrip('\n') for line in open('namedEntities')]
-i=0
+i=0+skip
 
 
 start =  t.process_time()
 time_delta =  t.process_time()
 for z in range(0,int(count/10)):
-    cursor = collection.find({"Year": ""+year+""}).skip(z*10).limit(10)
+    cursor = collection.find({"year": ""+year+""}).skip(skip+z*10).limit(10)
     for document in cursor:
-        text =document['Body']
+        text =document['body']
 
         sentences = nltk.sent_tokenize(text,language='english')
         bulk = tonkenz.initialize_unordered_bulk_op()
@@ -94,64 +95,69 @@ for z in range(0,int(count/10)):
             #     synsets = wn.synsets(lemma)
             #     if len(synsets) == 0:
             for token in tokenized_sent:
-                token = token.lower()
-                if token in stopwords.words('english') or token in ['(', ')', '-', '.', ',', ';', ':'] or re.match(
-                        '^[0-9\.,]*[0-9]$', token):
-                    continue
-                if wn.morphy(token) == None:
-                    delta_lemma = re.sub(r'[^\w\s]','',token)
-                    Capz=""
-                    isAuthor="false"
-                    containsNumbers="true"
-                    containsPunctuation="false"
-                    if token.lower() in punct:
-                        containsPunctuation=""
+                if re.match("^[a-zA-Z0-9]*$", token):
+                    Ltoken = token.lower()
+                    if Ltoken in stopwords.words('english') or token in ['(', ')', '-', '.', ',', ';', ':'] or re.match(
+                            '^[0-9\.,]*[0-9]$', token):
+                        continue
+                    if wn.morphy(Ltoken) == None:
+                        delta_lemma = re.sub(r'[^\w\s]','',Ltoken)
+                        Capz=""
+                        isAuthor="false"
+                        containsNumbers="true"
+                        containsPunctuation="false"
+                        if token.lower() in punct:
+                            containsPunctuation=""
 
-                    ### Schreibe Sonderzeichenwörter (wie z-buffering)
-                    # if lemma.lower() not in punct and lemma.lower() not in namendE:
-                    #     for token in punct:
-                    #         if token in lemma.lower() and len(lemma)>minlength:
-                    #             wwp.write(lemma+'\n')  # + '\t' + tag) # Alles mit einem Sonderzeichen.
+                        ### Schreibe Sonderzeichenwörter (wie z-buffering)
+                        # if lemma.lower() not in punct and lemma.lower() not in namendE:
+                        #     for token in punct:
+                        #         if token in lemma.lower() and len(lemma)>minlength:
+                        #             wwp.write(lemma+'\n')  # + '\t' + tag) # Alles mit einem Sonderzeichen.
 
-                    ### Schreibt Wörter die CAPZ sind
-                    if token.isupper() and len(token)>minlength:
-                        #iuc.write(lemma+'\n')
-                        Capz="upper"
-                        # if not hasNumbers(lemma):
-                        #     iucnn.write(lemma + '\n')
+                        ### Schreibt Wörter die CAPZ sind
+                        if token.isupper() and len(token)>minlength:
+                            #iuc.write(lemma+'\n')
+                            Capz="upper"
+                            # if not hasNumbers(lemma):
+                            #     iucnn.write(lemma + '\n')
 
-                    if token.islower() and len(token)>minlength:
-                        Capz="lower"
+                        if token.islower() and len(token)>minlength:
+                            Capz="lower"
 
-                    cursor_f = collection.find({"Authors.firstName": ""+token+""})
-                    cursor_l = collection.find({"Authors.lastName": ""+token+""})
+                        cursor_f = collection.find({"Authors.firstName": ""+token+""})
+                        cursor_l = collection.find({"Authors.lastName": ""+token+""})
 
-                    if cursor_f.retrieved>0 or cursor_l.retrieved>0:
-                        isAuthor="true"
+                        if cursor_f.retrieved>0 or cursor_l.retrieved>0:
+                            isAuthor="true"
 
 
-                    ### Schreibt wörter die Mixed sind
-                    if not token.isupper() and not token.islower() and len(token)>minlength:
-                        Capz="mixed"
-                        # winuanl.write(lemma+'\n')
-                    if not hasNumbers(token):
-                        containsNumbers="false"
-                    for token in punct:
-                        if token in token and len(token) > minlength:
-                            containsPunctuation="true"
-                        if not len(token) == len(delta_lemma):
-                            containsPunctuation = "true"
+                        ### Schreibt wörter die Mixed sind
+                        if not token.isupper() and not token.islower() and len(token)>minlength:
+                            Capz="mixed"
+                            # winuanl.write(lemma+'\n')
+                        if not hasNumbers(token):
+                            containsNumbers="false"
+                        for dogen in punct:
+                            if dogen in token and len(token) > minlength:
+                                containsPunctuation="true"
+                            if not len(token) == len(delta_lemma):
+                                containsPunctuation = "true"
 
-                    isAlreadyIn = tonkenz.find_one({"Token":""+token+"","document":document['_id']})
-                    if isAlreadyIn is None:
-                        bulk.insert({"Token": "" + token + "", "Capz": "" + Capz + "",
-                                           "containsNumbers": "" + containsNumbers + "",
-                                           "containsPunctuation": "" + containsPunctuation + "",
-                                           "isAuthor": ""+ isAuthor +"",
-                                            "length":  + len(token), "document":document['_id']})
-                        bulkSize=bulkSize+1
+                        isAlreadyIn = tonkenz.find_one({"Token":""+token+"","document":document['_id']})
+                        #isAlreadyInBulk = bulk.find({"Token":""+token+"","document":document['_id']})
 
-                            #print(lemma.lower())
+                        if isAlreadyIn is None:
+                            #print(token)
+                            bulk.insert({"Token": "" + token + "", "cap": "" + Capz + "",
+                                               "containsNumbers": "" + containsNumbers + "",
+                                               "containsPunctuation": "" + containsPunctuation + "",
+                                               "isAuthor": ""+ isAuthor +"",
+                                                "length":  + len(token), "document":document['_id'], "paperName":document['journalName'],
+                                                "path2file":document['path2file']})
+                            bulkSize=bulkSize+1
+
+                                #print(lemma.lower())
         if bulkSize>0:
             bulk.execute()
         s = str(i)
@@ -163,6 +169,7 @@ for z in range(0,int(count/10)):
         i=i+1
         bulkSize=0
 
+#nltk.nae
  # def findIfItsAnAuthorName(collection, str ):
  #     cursor = collection.find({"Authors.firstName": ""+str+""}).limit(1)
  #     if len(cursor) == 0:
